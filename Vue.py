0340cfcb58d05955f0c8c30dd64800524c0bcfba
antoine <antoine.lutsen@gmail.com>
2025-06-11 15:47:43 +0200

    modif_secteur_v3

--- Vue.py
+++ Vue.py
@@ -1,169 +1,276 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import (
+    QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton,
+    QTextEdit, QScrollArea, QFrame
+)
 from PyQt6.QtGui import QPixmap, QPainter, QColor, QPen, QBrush
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, pyqtSignal
+
 
 class VuePlan(QWidget):
+    celluleCliquee = pyqtSignal(int, int)
+
     def __init__(self, image_path: str, scale_percent: int = 100, cell_size: int = 8):
         super().__init__()
+        self.setWindowTitle("Plan avec interface détaillée")
 
-        self.setWindowTitle("Plan quadrillé avec secteurs par cases")
         self.image_path = image_path
         self.scale_percent = scale_percent
         self.cell_size = cell_size
 
         self.pixmap_original = QPixmap(self.image_path)
-
         screen = QApplication.primaryScreen().availableGeometry()
-        screen_width = screen.width()
-        screen_height = screen.height()
-
-        new_width = int(screen_width * (self.scale_percent / 100))
-        new_height = int(screen_height * (self.scale_percent / 100))
-
+        new_width = 1200
+        new_height = 1000
         self.pixmap_scaled = self.pixmap_original.scaled(
             new_width, new_height, Qt.AspectRatioMode.KeepAspectRatio
         )
 
-        self.setFixedSize(self.pixmap_scaled.size())
+        main_layout = QHBoxLayout(self)
 
-        self.inaccessible_cells = {
-            (5, 10), (7, 12)
-        }
+        self.plan_frame = QFrame()
+        self.plan_frame.setMinimumSize(self.pixmap_scaled.size())
+        main_layout.addWidget(self.plan_frame)
 
+        right_panel = QVBoxLayout()
+        main_layout.addLayout(right_panel)
+
+        titre = QLabel("Description")
+        titre.setStyleSheet("font-size: 16px; font-weight: bold;")
+        texte = QLabel("Bienvenue sur le plan du magasin. Cliquez sur une zone pour voir les produits.")
+        texte.setWordWrap(True)
+        texte.setMaximumHeight(100)
+        right_panel.addWidget(titre, stretch=0)
+        right_panel.addWidget(texte, stretch=1)
+
+        self.zone_liste = QVBoxLayout()
+        scroll_content = QWidget()
+        scroll_content.setLayout(self.zone_liste)
+
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setWidget(scroll_content)
+        right_panel.addWidget(scroll_area, stretch=4)
+
+        self.bouton_ajout = QPushButton("+ Ajouter un produit")
+        self.bouton_ajout.clicked.connect(self.ajouter_champ_produit)
+        right_panel.addWidget(self.bouton_ajout, stretch=0)
+
+        self.inaccessible_cells = {(5, 10), (7, 12)}
         self.sectors = {
     "Fruits et Legumes": {
         "color": QColor(144, 238, 144, 120),
-        "cells": {(row, col) for row in range(13, 49) for col in range(31, 50)}
+        "cells": {(row, col) for row in range(11, 53) for col in range(32, 52)}
     },
-    "Charcuterie": {
+    "Charcuterie LS": {
         "color": QColor(255, 182, 193, 120),
-        "cells": {(row, col) for row in range(13, 48) for col in range(28, 30)},
-        "cells2": {(row, col) for row in range(31, 48) for col in range(23, 24)}
+        "cells": {(row, col) for row in range(11, 53) for col in range(28, 32)},  
     },
     "Poissonnerie": {
         "color": QColor(135, 206, 250, 120),
-        "cells": {(row, col) for row in range(2, 12) for col in range(2, 13)}
+        "cells": {(row, col) for row in range(1, 14) for col in range(0, 17)}
     },
-    "Fromage": {
+    "Fromage Trad": {
         "color": QColor(255, 255, 153, 120),
-        "cells": {(row, col) for row in range(2, 7) for col in range(19, 34)}
+        "cells": {(row, col) for row in range(1, 9) for col in range(18, 36)}
     },
     "Surgeles": {
         "color": QColor(173, 216, 230, 120),
-        "cells": {(row, col) for row in range(12, 49) for col in range(50, 63)}
+        "cells": {(row, col) for row in range(11, 53) for col in range(52, 67)}
     },
     "Epicerie": {
         "color": QColor(240, 230, 140, 120),
-        "cells": {(row, col) for row in range(55, 87) for col in range(28, 57)}
+        "cells": {(row, col) for row in range(54, 93) for col in range(29, 59)}
     },
     "Eaux": {
         "color": QColor(135, 206, 235, 120),
-        "cells": {(row, col) for row in range(55, 87) for col in range(10, 13)}
+        "cells": {(row, col) for row in range(54, 93) for col in range(9, 13)}
     },
     "Cave a vins": {
         "color": QColor(199, 21, 133, 120),
-        "cells": {(row, col) for row in range(55, 73) for col in range(13, 27)}
+        "cells": {(row, col) for row in range(54, 76) for col in range(13, 29)}
     },
     "Textile": {
         "color": QColor(216, 191, 216, 120),
-        "cells": {(row, col) for row in range(55, 87) for col in range(57, 75)}
+        "cells": {(row, col) for row in range(54, 93) for col in range(59, 102)}
     },
     "Bazar": {
         "color": QColor(255, 215, 0, 120),
-        "cells": {(row, col) for row in range(28, 49) for col in range(112, 134)}
+        "cells": {(row, col) for row in range(29, 51) for col in range(112, 142)}
     },
     "Entretien": {
         "color": QColor(224, 255, 255, 120),
-        "cells": {(row, col) for row in range(14, 28) for col in range(112, 134)}
+        "cells": {(row, col) for row in range(15, 29) for col in range(112, 142)}
     },
     "Saisonnier": {
         "color": QColor(176, 224, 230, 120),
-        "cells": {(row, col) for row in range(12, 49) for col in range(97, 101)}
+        "cells": {(row, col) for row in range(13, 51) for col in range(102, 110)}
     },
     "Parfumerie": {
         "color": QColor(255, 192, 203, 120),
-        "cells": {(row, col) for row in range(12, 49) for col in range(85, 98)}
+        "cells": {(row, col) for row in range(11, 54) for col in range(88, 102)}
     },
     "Pain / Patisserie": {
         "color": QColor(255, 228, 181, 120),
-        "cells": {(row, col) for row in range(12, 14) for col in range(112, 134)}
+        "cells": {(row, col) for row in range(11, 15) for col in range(115, 142)}
     },
     "Jus de fruits": {
         "color": QColor(255, 160, 122, 120),
-        "cells": {(row, col) for row in range(94, 100) for col in range(6, 32)}
+        "cells": {(row, col) for row in range(94, 106) for col in range(6, 34)}
     },
     "Boutique Informatique Telephonie": {
         "color": QColor(100, 149, 237, 120),
-        "cells": {(row, col) for row in range(97, 108) for col in range(123, 141)}
+        "cells": {(row, col) for row in range(101, 112) for col in range(128, 149)}
     },
     "Ultra Frais": {
         "color": QColor(173, 216, 230, 120),  # bleu clair
-        "cells": {(row, col) for row in range(15, 49) for col in range(2, 13)}
+        "cells": {(row, col) for row in range(14, 54) for col in range(0, 13)}
     },
     "Bieres": {
         "color": QColor(144, 238, 144, 120),  # vert
-        "cells": {(row, col) for row in range(55, 100) for col in range(3, 6)}
+        "cells": {(row, col) for row in range(55, 106) for col in range(0, 6)}
     },
     "Alcools": {
         "color": QColor(255, 0, 0, 120),  # rouge
-        "cells": {(row, col) for row in range(73, 88) for col in range(13, 16)}
+        "cells": {(row, col) for row in range(76, 93) for col in range(13, 18)}
     },
     "Lait + oeufs": {
         "color": QColor(0, 191, 255, 120),  # bleu (deep sky blue)
-        "cells": {(row, col) for row in range(23, 49) for col in range(20, 23)}
+        "cells": {(row, col) for row in range(24, 53) for col in range(18, 24)}
     },
     "Saucissons": {
         "color": QColor(70, 130, 180, 120),  # bleu (steel blue)
-        "cells": {(row, col) for row in range(12, 23) for col in range(20, 23)}
+        "cells": {(row, col) for row in range(11, 24) for col in range(19, 24)}
     },
     "Saucisserie": {
         "color": QColor(139, 0, 0, 120),  # rouge foncé (dark red)
-        "cells": {(row, col) for row in range(12, 31) for col in range(22, 25)}
+        "cells": {(row, col) for row in range(11, 32) for col in range(24, 27)}
     },
     "Volailles": {
         "color": QColor(255, 69, 0, 120),  # rouge (orange red)
-        "cells": {(row, col) for row in range(1, 5) for col in range(35, 50)}
+        "cells": {(row, col) for row in range(1, 7) for col in range(36, 53)}
     },
     "Boucherie": {
         "color": QColor(220, 20, 60, 120),  # rouge (crimson)
-        "cells": {(row, col) for row in range(1, 5) for col in range(54, 80)}
+        "cells": {(row, col) for row in range(1, 6) for col in range(54, 83)}
     },
     "Boucherie trad": {
         "color": QColor(178, 34, 34, 120),  # rouge (firebrick)
-        "cells": {(row, col) for row in range(5, 8) for col in range(81, 97)}
+        "cells": {(row, col) for row in range(4, 10) for col in range(84, 101)}
     },
     "Rotisserie": {
         "color": QColor(255, 0, 0, 120),  # rouge (red)
-        "cells": {(row, col) for row in range(5, 8) for col in range(97, 115)}
+        "cells": {(row, col) for row in range(4, 10) for col in range(101, 120)}
     },
     "Charcuterie": {
         "color": QColor(255, 165, 0, 120),  # orange
-        "cells": {(row, col) for row in range(5, 8) for col in range(115, 134)}
+        "cells": {(row, col) for row in range(4, 10) for col in range(120, 141)}
     },
     "Lessives": {
         "color": QColor(148, 0, 211, 120),  # violet (dark violet)
-        "cells": {(row, col) for row in range(12, 37) for col in range(139, 141)}
+        "cells": {(row, col) for row in range(11, 39) for col in range(142, 147)}
     },
     "Patisserie industrielle / Pain": {
         "color": QColor(255, 140, 0, 120),  # orange (dark orange)
-        "cells": {(row, col) for row in range(12, 49) for col in range(66, 68)}
+        "cells": {(row, col) for row in range(11, 53) for col in range(67, 71)}
     },
     "Univers Animaux": {
         "color": QColor(255, 255, 0, 120),  # jaune
-        "cells": {(row, col) for row in range(12, 49) for col in range(68, 77)}
+        "cells": {(row, col) for row in range(11, 53) for col in range(71, 80)}
     },
     "Univers Bebe": {
         "color": QColor(255, 182, 193, 120),  # rose (light pink)
-        "cells": {(row, col) for row in range(12, 49) for col in range(76, 85)}
+        "cells": {(row, col) for row in range(11, 53) for col in range(80, 88)}
+    },
+    "Fromage": {
+        "color": QColor(255, 182, 193, 120),  # rose (light pink)
+        "cells": {(row, col) for row in range(14, 53) for col in range(13, 18)}
+    },
+    "Sortie": {
+        "color": QColor(255, 182, 193, 120),  # rose (light pink)
+        "cells": {(row, col) for row in range(105, 106) for col in range(36, 99)}
     }
 }
+        
+        extension_charcuterie = {(row, col) for row in range(32, 53) for col in range(24, 28)}  
+        self.sectors["Charcuterie LS"]["cells"] = self.sectors["Charcuterie LS"]["cells"].union(extension_charcuterie)
+
+        extension_bazar = {(row, col) for row in range(65, 101) for col in range(112, 149)}  
+        self.sectors["Bazar"]["cells"] = self.sectors["Bazar"]["cells"].union(extension_bazar)
+
+        extension_bazar2 = {(row, col) for row in range(39, 65) for col in range(142, 149)}  
+        self.sectors["Bazar"]["cells"] = self.sectors["Bazar"]["cells"].union(extension_bazar2)
+
+        extension_saisonnier = {(row, col) for row in range(54, 94) for col in range(102, 110)}  
+        self.sectors["Saisonnier"]["cells"] = self.sectors["Saisonnier"]["cells"].union(extension_saisonnier)
+
+        extension_saisonnier2 = {(row, col) for row in range(51, 65) for col in range(112, 142)}  
+        self.sectors["Saisonnier"]["cells"] = self.sectors["Saisonnier"]["cells"].union(extension_saisonnier2)
+
+        extension_epicerie = {(row, col) for row in range(76, 93) for col in range(22, 29)}  
+        self.sectors["Epicerie"]["cells"] = self.sectors["Epicerie"]["cells"].union(extension_epicerie)
+
+        extension_vin = {(row, col) for row in range(76, 93) for col in range(18, 22)}  
+        self.sectors["Cave a vins"]["cells"] = self.sectors["Cave a vins"]["cells"].union(extension_vin)
+
+        self.info_label = QLabel(self.plan_frame)
+        self.info_label.setStyleSheet("""
+            background-color: rgba(255, 255, 255, 230);
+            border: 1px solid gray;
+            padding: 5px;
+            font-size: 11px;
+        """)
+        self.info_label.setWordWrap(True)
+        self.info_label.setVisible(False)
 
         self.show()
 
+    def ajouter_champ_produit(self):
+        champ = QTextEdit()
+        champ.setFixedHeight(40)
+        champ.textChanged.connect(lambda: self.limiter_texte(champ))
+        self.zone_liste.addWidget(champ)
+
+    def limiter_texte(self, champ):
+        texte = champ.toPlainText()
+        if len(texte) > 100:
+            champ.setPlainText(texte[:100])
+            cursor = champ.textCursor()
+            cursor.setPosition(100)
+            champ.setTextCursor(cursor)
+
+    def afficher_info_zone(self, texte: str, x: int, y: int):
+        self.info_label.setText(texte)
+        self.info_label.adjustSize()
+        label_width = self.info_label.width()
+        label_height = self.info_label.height()
+        x = min(x, self.plan_frame.width() - label_width)
+        y = min(y, self.plan_frame.height() - label_height)
+        self.info_label.move(x, y)
+        self.info_label.setVisible(True)
+
+    def cacher_info_zone(self):
+        self.info_label.setVisible(False)
+
+    def afficher_produits_secteur(self, secteur: str, produits: list[str], x: int, y: int):
+        self.cacher_info_zone()
+        texte = f"<b>Secteur : {secteur}</b><br>" + "<br>".join(produits) if produits else f"<b>Secteur : {secteur}</b><br>Aucun produit trouvé"
+        self.info_label.setText(texte)
+        self.info_label.adjustSize()
+        label_width = self.info_label.width()
+        label_height = self.info_label.height()
+        x = min(x, self.plan_frame.width() - label_width)
+        y = min(y, self.plan_frame.height() - label_height)
+        self.info_label.move(x, y)
+        self.info_label.setVisible(True)
+
+    def vider_produits_secteur(self):
+        self.cacher_info_zone()
+
+
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.drawPixmap(self.rect(), self.pixmap_scaled)
+        painter.drawPixmap(0, 0, self.pixmap_scaled)
 
         width = self.pixmap_scaled.width()
         height = self.pixmap_scaled.height()
@@ -171,29 +278,20 @@ class VuePlan(QWidget):
         pen_grid = QPen(QColor(0, 255, 0, 180))
         pen_grid.setWidth(1)
         painter.setPen(pen_grid)
-
         for x in range(0, width, self.cell_size):
             painter.drawLine(x, 0, x, height)
-
         for y in range(0, height, self.cell_size):
             painter.drawLine(0, y, width, y)
 
-        brush_black = QColor(0, 0, 0, 255)
-        painter.setBrush(brush_black)
+        painter.setBrush(QColor(0, 0, 0))
         painter.setPen(Qt.PenStyle.NoPen)
         for (row, col) in self.inaccessible_cells:
-            rect_x = col * self.cell_size
-            rect_y = row * self.cell_size
-            painter.drawRect(rect_x, rect_y, self.cell_size, self.cell_size)
+            painter.drawRect(col * self.cell_size, row * self.cell_size, self.cell_size, self.cell_size)
 
-        # Draw sectors cases with colors
         for sector in self.sectors.values():
             painter.setBrush(QBrush(sector["color"]))
-            painter.setPen(Qt.PenStyle.NoPen)
             for (row, col) in sector["cells"]:
-                rect_x = col * self.cell_size
-                rect_y = row * self.cell_size
-                painter.drawRect(rect_x, rect_y, self.cell_size, self.cell_size)
+                painter.drawRect(col * self.cell_size, row * self.cell_size, self.cell_size, self.cell_size)
 
     def mousePressEvent(self, event):
         pos = event.position().toPoint()
@@ -201,11 +299,13 @@ class VuePlan(QWidget):
         row = pos.y() // self.cell_size
         if (row, col) in self.inaccessible_cells:
             return
-        print(f"Clicked at x={pos.x()}, y={pos.y()}")
-        print(f"Grid cell clicked: row={row}, col={col}")
+        # print(f"Clicked at x={pos.x()}, y={pos.y()}")
+        # print(f"Grid cell clicked: row={row}, col={col}")
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = VuePlan("plan.jpg", scale_percent=95, cell_size=8)
+    window = VuePlan("plan.jpg", scale_percent=90, cell_size=8)
     window.show()
     sys.exit(app.exec())
